2188a69cab4c4283eddddd895b401132d9c032cd
ginkgoch <ginkgoch@outlook.com>
2018-07-28 18:20:16 +0800

    implemented show records

--- commands/showRecords.js
+++ commands/showRecords.js
@@ -1,5 +1,7 @@
 const _ = require('lodash');
+const table = require('table').table;
 const Shapefile = require('ginkgoch-shapefile-reader');
+const truncateOption = { length: 30 };
 
 async function loadRecords(file, cmd) {
     const limit = _.isUndefined(cmd.limit) ? 10 : cmd.limit;
@@ -11,27 +13,56 @@ async function loadRecords(file, cmd) {
         const records = await shapefile.iterator(columns);
         let record = undefined;
         let counter = 0;
-        while((record = await records.next()) && !record.done) {
+        let headers = undefined;
+        let tableData = undefined;
+
+        while ((record = await records.next()) && !record.done) {
             record = _.omit(record, ['done']);
-            if(!geom) {
+            if (!geom) {
                 record = _.omit(record, ['geom', 'envelope']);
             }
 
-            console.log(JSON.stringify(record));
+            if (cmd.pretty) {
+                if (!headers) {
+                    headers = _.keys(record.fields);
+                    cmd.geom && headers.push('geom');
+                    tableData = [ headers ];
+                }
+
+                let row = _.values(record.fields);
+                cmd.geom && row.push(_.truncate(JSON.stringify(record.geom), truncateOption));
+                tableData.push(row);
+            } else {
+                console.log(JSON.stringify(record));
+                console.log();
+            }
             counter++;
 
-            if(limit !== 0 && counter >= limit) {
+            if (limit !== 0 && counter >= limit) {
                 break;
             }
         }
 
-        const count = await shapefile.count();
-        if(count > counter) {
-            console.log(`Reading complete. ${count - counter}/${count} record(s) read`);
+        if (tableData) {
+            console.log(table(tableData));
         }
+
+        const count = await shapefile.count();
+        if (count > counter) {
+            console.log();
+            console.log(`Reading complete. ${counter}/${count} record(s)`);
+        }
+
+        console.log();
+        console.log(`Tips:`);
+        console.log(` - set option -l, --limit to 0 to read all records`);
+        console.log(` - use option -g, --geom to 0 to read all records with geometry`);
+        console.log(` - use option -p, --pretty to return data with pretty table format`);
+        console.log(` - use option -c, --column to return necessary fields. e.g. -c field1,field2 returns two fields`);
+
     });
 }
 
-module.exports = function(file, cmd) {
+module.exports = function (file, cmd) {
     loadRecords(file, cmd);
 }
--- index.js
+++ index.js
@@ -12,14 +12,15 @@ program.command('show-header <file>')
 
 program.command('show-fields <file>')
     .description('output a specified shapefile fields information')
-    .option('-p, --pretty', 'output header with a pretty table format')
+    .option('-p, --pretty', 'output fields with a pretty table format')
     .action(require('./commands/showFields'));
 
 program.command('show-records <file>')
     .description('output a specified shapefile records information')
     .option('-l, --limit <n>', 'returning records limit. accept any number. 0 means all records. default to 10', parseInt)
     .option('-c, --columns <items>', 'returning columns include in the results. Multiple columns are supported by separater ",". Default to all columns', val => val.split(','))
-    .option('-g, --geom', 'includes geometry in the returned table')
+    .option('-g, --geom', 'includes geometry in the returned content')
+    .option('-p, --pretty', 'output records with a pretty table format')
     .action(require('./commands/showRecords'));
 
 program.command('convert <file>')
@@ -27,7 +28,7 @@ program.command('convert <file>')
     .option('-t, --type <value>', 'destination type to convert to. Supported types are json|csv|png')
     .option('-o, --output <value>', 'output file path. If only directory is specified, the same file name will be used. Default to the same folder of the source Shapefile')
     .action((file, cmd) => {
-        console.log(file, cmd.type, cmd.columns.output);
+        console.log('unsupported yet! contact ginkgoch@outlook.com if needed');
     });
 
 program.parse(process.argv);
